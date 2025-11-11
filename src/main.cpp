294e9614e456a0479abf22f2c49f862bd3fbb33c
hhhwrfssyhdsy <2190492405@qq.com>
2025-11-11 19:57:29 +0800

    second-stage

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,10 @@
 
 int main() {
   // Flush after every std::cout / std:cerr
+  std::string command;
+  std::getline(std::cin,command);
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
+  std::cout << command << ": command not found" << std::endl;
+  
 }
