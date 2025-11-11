73fe389726f69ccd3aa2ab53360f169d73099776
hhhwrfssyhdsy <2190492405@qq.com>
2025-11-11 20:03:47 +0800

    second-stage

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 int main() {
   // Flush after every std::cout / std:cerr
   std::string command;
-  std::getline(std::cin,command);
   std::cout << "$ "; 
+  std::getline(std::cin,command);
   std::cerr << std::unitbuf;
   std::cout << command << ": command not found" << std::endl;
 }
